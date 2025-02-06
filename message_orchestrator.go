9948bdc5faa06b2c6ae6d0cb6c66623ce7d874a4
Liam Marcassa <markassa@gmail.com>
2025-02-06 14:31:21 -0500

    Add more detail on how to handle the long-running process of determining if a message is a palindrome

--- message_orchestrator.go
+++ message_orchestrator.go
@@ -13,18 +13,20 @@ type Message struct {
 	hash string // a hash of the text, for quick comparison
 	text string
 	isPalindrome int
+	stopChan chan bool
 }
 
-func (mo *MessageOrchestrator) Add(text string) (ID, chan bool) {
+// returns id, stopChan, and a channel that will be closed when the isPalindrome calculation is complete
+func (mo *MessageOrchestrator) Add(text string) (ID, chan bool, chan bool) {
 	// atomically read and increment ss.nextId
 	// create hash of text
-	// make a new Message
+	// make a new Message, with a stopChan
 	// save to ss.messages
 
-	// create a channel
-	// spawn a goroutine to: calculate isPalindrome, update (locked) or don't update if hash has changed / item deleted, then close the channel
+	// create a doneChan
+	// spawn a goroutine to: calculate isPalindrome (check stopChan periodically), update (locked) or don't update if hash has changed / item deleted, then close the doneChan
 
-	return 0, nil
+	return 0, nil, nil
 }
 
 func (mo *MessageOrchestrator) Get(id ID) (Message, bool) {
@@ -34,17 +36,20 @@ func (mo *MessageOrchestrator) Get(id ID) (Message, bool) {
 	return Message{}, false
 }
 
-func (mo *MessageOrchestrator) Update(id ID, text string) (bool, chan bool) {
+// returns success, stopChan, and a channel that will be closed when the isPalindrome calculation is complete
+func (mo *MessageOrchestrator) Update(id ID, text string) (bool, chan bool, chan bool) {
 	// calculate the new text hash
+	// create a new stopChan
 	// obtain a write lock
 	// check if item exists, if not, then return false
-	// update text and hash, then drop the lock
+	// close existing stopChan if present
+	// update text, hash, isPalindrome, stopChan, then drop the lock
 	// read (locked) from ss.messages
 
-	// create a channel
-	// spawn a goroutine to: calculate isPalindrome, update (locked) or don't update if hash has changed / item deleted, then close the channel
+	// create a doneChan
+	// spawn a goroutine to: calculate isPalindrome (check stopChan periodically), update (locked) or don't update if hash has changed / item deleted, then close the doneChan
 
-	return false, nil
+	return false, nil, nil
 }
 
 func (mo *MessageOrchestrator) Delete(id ID) bool {
